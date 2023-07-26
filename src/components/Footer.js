e9dce4335bf58c42a9e4d5a7124172640fd05de5
newton-w <71337527+newton-w@users.noreply.github.com>
2023-07-26 12:34:33 +0300

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
     return (
         <footer>
              
-            Challenge by <a href="#" rel="noreferrer">&nbsp; Frontend Mentor</a>.
-            Coded by <a href="#" target="_blank" rel="noreferrer">&nbsp;Nyiha</a>.
+            Challenge by <a href="/" rel="noreferrer">&nbsp; Frontend Mentor</a>.
+            Coded by <a href="/" target="_blank" rel="noreferrer">&nbsp;Nyiha</a>.
         </footer>
     )
 }
