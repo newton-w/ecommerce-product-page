944b48caccf9df7c2cd4b2766a62ef6bc082f333
newton-w <71337527+newton-w@users.noreply.github.com>
2024-02-09 20:07:44 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer>
              
             Challenge by <a href="/" rel="noreferrer">&nbsp; Frontend Mentor</a>.
-            Coded by <a href="/" target="_blank" rel="noreferrer">&nbsp;Nyiha</a>.
+            Coded by <a href="/" target="_blank" rel="noreferrer">&nbsp;Newton</a>.
         </footer>
     )
 }
